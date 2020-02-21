55d9085380310c6f70efa62beeabf3903c9f9fda
raveendra.tudangil <raveendra@greayatom.com>
2020-02-21 17:02:06 +0530

    Arrays

---
+++ 03 Classes Objects and their Members/demo5/BasicsDemo.java
@@ -0,0 +1,17 @@
+package demo5;
+
+public class BasicsDemo {
+
+
+    static void arrays(){
+        System.out.println("\nInside Arrays");
+        int [] myArray = new int [] {9, 11, 2, 5, 4, 4, 6};
+        System.out.println("myArray.length: "+myArray.length);
+        System.out.println("myArray [1] : "+myArray [1]);
+        System.out.println("myArray [7] : "+myArray [7]); // Throws ArrayIndexOutOfBoundsException
+    }
+
+    public static void main(String[] args) {
+        arrays();
+    }
+}
