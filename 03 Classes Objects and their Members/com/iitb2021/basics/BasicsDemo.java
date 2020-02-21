52e02a1e20a098cf8daaa3d6757fea8c25647646
raveendra.tudangil <raveendra@greayatom.com>
2020-02-21 12:14:33 +0530

    1. HelloWorld 2. BasicsDemo

---
+++ 03 Classes Objects and their Members/com/iitb2021/basics/BasicsDemo.java
@@ -0,0 +1,20 @@
+package com.iitb2021.basics;
+
+public class BasicsDemo {
+
+
+    static void print(){
+        System.out.println("\n\nInside print ...");
+        System.out.println("Hello, world!!");           // Advance cursor to the beginning of the next line
+        System.out.println();                           // Print empty line
+        System.out.print("Hello, World!!");             // Cursor stayed after the printed string
+        System.out.println("Hello,");                   // Print a space
+        System.out.print(" ");
+        System.out.println("world!!");
+
+    }
+
+    public static void main(String[] args) {
+       print();
+    }
+}
